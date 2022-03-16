ff5a214f820bbfa9fdf855ae575b1c6f65ef81dc
Paige Godfrey <paagodfrey@gmail.com>
2022-03-15 18:40:35 -0700

    getCount final

--- getCount.js
+++ getCount.js
@@ -1,20 +1,45 @@
+/* 
+Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels (but not y).
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+const stringValid = str => {
+  return str && typeof str === 'string';
+}
+
+// O(n) time
+// O(1) space
 const getCount = str => {
-  if (!str || typeof str !== "string") {
-    return 'error - must input valid string';
-  }
+  if (!stringValid) return 'error - must input valid string';
 
   let result = 0;
   let vowels = 'aeiou';
 
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < vowels.length; j++) {
-      if (str[i] === vowels[j]) result++;
-      console.log(vowels[j]);
-      continue;
-    }
+  for (let char of str) {
+    if (vowels.includes(char)) result++;
   }
 
   return result;
 }
 
-// getCount('abcdef') => 2
+// without using includes method
+// O(n) time
+// O(1) space
+const getCountAlt = str => {
+  if (!stringValid(str)) return 'error - must input valid string';
+
+  let result = 0;
+  let vowels = 'aeiou';
+
+  for (let char of str) {
+    for (let vowel of vowels) {
+      if (char === vowel) {
+        result++;
+        continue;
+      }
+    }
+  }
+  
+  return result;
+}
